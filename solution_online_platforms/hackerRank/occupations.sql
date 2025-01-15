0abaf25f4942b8d7a8cfca3d852b7c4aad2e5088
Shikhar Mishra <shikharmishra6350650@gmail.com>
2025-01-15 12:52:35 +0530

    add optimized solution

--- solution_online_platforms/hackerRank/occupations.sql
+++ solution_online_platforms/hackerRank/occupations.sql
@@ -37,3 +37,21 @@ from Doctors d
 full join Professors p on d.doc_rnk=p.prof_rnk
 full join Singers s on d.doc_rnk=s.sing_rnk or p.prof_rnk=s.sing_rnk
 full join Actors a on d.doc_rnk=a.act_rnk or p.prof_rnk=a.act_rnk or s.sing_rnk=a.act_rnk
+
+above solution only works in sql server bcz it already sort row_number() column
+-- ===============================================================
+
+more optimized solution...
+
+with RankedData as(
+    select Name, Occupation, row_number() over(partition by Occupation order by Name) as Rnk
+    FROM Occupations
+)
+select
+    max(case when Occupation = 'Doctor' then Name end) as Doctor,
+    max(case when Occupation = 'Professor' then Name end) as Professor,
+    max(case when Occupation = 'Singer' then Name end) as Singer,
+    max(case when Occupation = 'Actor' then Name end) as Actor
+from RankedData
+group by Rnk
+order by Rnk;
